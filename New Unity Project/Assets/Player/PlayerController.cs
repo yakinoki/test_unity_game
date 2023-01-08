308eafb25c508953d7188998dc58e293eb9ff730
unknown <yakinoki>
2023-01-08 22:11:38 +0900

    update_code

--- New Unity Project/Assets/Player/PlayerController.cs
+++ New Unity Project/Assets/Player/PlayerController.cs
@@ -8,6 +8,11 @@ public class PlayerController : MonoBehaviour
     float axisH = 0.0f;
     public float speed = 3.0f;
 
+    public float jump = 6.0f;
+    public LayerMask groundLayer;
+    bool goJump = false;
+    bool onGround = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,10 +33,35 @@ public class PlayerController : MonoBehaviour
             Debug.Log("left move");
             transform.localScale = new Vector2(-1, 1);
         }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
     }
 
     void FixedUpdate()
     {
-        rbody.velocity = new Vector2(speed * axisH, rbody.velocity.y);
+        onGround = Physics2D.Linecast(transform.position,
+                                       transform.position - (transform.up * 0.1f),
+                                       groundLayer);
+        if (onGround || axisH != 0)
+        {
+            rbody.velocity = new Vector2(speed * axisH, rbody.velocity.y);
+        }
+        if (onGround && goJump)
+        {
+            Debug.Log("Jump");
+            Vector2 jumpPw = new Vector2(0, jump);
+            rbody.AddForce(jumpPw, ForceMode2D.Impulse);
+            goJump = false;
+        }
+        
+    }
+
+    public void Jump()
+    {
+        goJump = true;
+        Debug.Log("Jumpbutton");
     }
 }
